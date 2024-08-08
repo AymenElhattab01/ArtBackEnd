0bb434c2cbbfda8d3cbd05ccd3770ebd1b66a640
AymenElhattab01 <aymenelhattab@gmail.com>
2024-08-08 21:16:55 +0100

    first commit

---
+++ src/main/java/com/example/artbackend/Entities/SousAtelier.java
@@ -0,0 +1,35 @@
+package com.example.artbackend.Entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class SousAtelier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name ;
+    private String Description ;
+    @ManyToOne
+
+    private Atelier atelier;
+    private Float prix ;
+
+
+    @ManyToOne
+    private Utilisateur utilisateur;
+
+
+    @OneToMany(mappedBy = "sousAtelier")
+    private List<Media> media;
+
+
+}
