06bf61fc63ba552de051dd15bd1ac0dea68fbfe2
AymenElhattab01 <aymenelhattab@gmail.com>
2024-08-08 21:25:35 +0100

    First Commit Aymen

---
+++ src/main/java/com/example/artbackend/Entities/Formation.java
@@ -0,0 +1,39 @@
+package com.example.artbackend.Entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Formation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name ;
+    private String Description ;
+    private String Formateur ;
+    private Date Debut ;
+    private Date Fin ;
+    private int  heures ;
+    private Float prix ;
+
+
+    @ManyToOne
+
+    private Utilisateur utilisateur ;
+
+    @OneToMany(mappedBy = "formation")
+    private  List<Media> media;
+
+
+
+
+}
